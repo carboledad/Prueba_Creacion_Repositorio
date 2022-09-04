bf4367c18c51da32a2ebf9493279c316d9644034
Cesar Augusto Arboleda Diosa <101777132+carboledad@users.noreply.github.com>
2022-09-04 12:25:59 -0500

    Segundo_python_local
    Se carga un archivo local de python

---
+++ Laboratorio_1_Python.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+sma_data = pd.read_csv("https://raw.githubusercontent.com/dphi-official/Datasets/master/Standard_Metropolitan_Areas_Data-data.csv", sep=",")
+
+sma_data.describe(include = 'all')
